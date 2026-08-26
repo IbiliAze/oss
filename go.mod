85237e9d986b942aa203d4b4100acec1e439ab8a
Ibi Hasanli <ibili73@gmail.com>
2026-08-26 16:44:58 +0100

    Implement Config.Load using koanf to read YAML and env overrides
    Wire vaultlet.yaml and VAULTLET_ env vars into Config via koanf,
    replacing the previous stub that returned an empty struct. Add
    bitwarden connection fields to the default config file so they can
    be populated via env or file overrides.

--- go.mod
+++ go.mod
@@ -9,8 +9,14 @@ require (
 
 require (
 	github.com/bitwarden/sdk-go/v2 v2.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/knadh/koanf v1.5.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260526163538-3dc84a4a5aaa // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,14 @@
 package config
 
-import "github.com/IbiliAze/vaultlet/internal/adapters/bitwarden"
+import (
+	"strings"
+
+	"github.com/IbiliAze/vaultlet/internal/adapters/bitwarden"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+)
 
 type Config struct {
 	Listen    string           `koanf:"listen"`
@@ -9,5 +17,14 @@ type Config struct {
 }
 
 func Load() (Config, error) {
-	return Config{}, nil
+	k := koanf.New(".")
+	k.Load(file.Provider("vaultlet.yaml"), yaml.Parser())
+	k.Load(env.Provider("VAULTLET_", ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(strings.TrimPrefix(s, "VAULTLET_")), "_", ".")
+	}), nil)
+
+	var cfg Config
+	k.Unmarshal("", &cfg)
+
+	return cfg, nil
 }
